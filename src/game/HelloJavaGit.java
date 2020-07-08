22cbd027990c47f346adb6cdb83bc758f9f24dc4
dmitiy-M <mishchukltd@gmail.com>
2020-07-08 20:30:30 +0300

    Second commit

--- src/game/HelloJavaGit.java
+++ src/game/HelloJavaGit.java
@@ -3,5 +3,6 @@ package game;
 public class HelloJavaGit {
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        System.out.println("Hello World");
     }
 }
